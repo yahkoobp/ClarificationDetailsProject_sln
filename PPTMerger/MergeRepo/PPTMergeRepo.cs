a95ccdc3860645ec82e1a81ae3364e701006ff6b
yahkoobp <yahkoobp007@gmail.com>
2024-12-03 00:10:25 +0530

    updatedd

--- PPTMerger/MergeRepo/PPTMergeRepo.cs
+++ PPTMerger/MergeRepo/PPTMergeRepo.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Windows;
 using Microsoft.Office.Core;
 using Microsoft.Office.Interop.PowerPoint;
 using PPTMerger.Delegates;
